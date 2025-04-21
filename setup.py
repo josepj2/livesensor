9d073ef4b98e4381370d9d57ff8fadf8c7efbf00
Jason Joseph <josepj2@gmail.com>
2025-04-21 14:42:33 +0530

    Added project files

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+def get_requirements() -> list [str]:
+    requirements_list = list [str] = []
+    return requirements_list
+
+
+setup(
+name="sensor",
+version = "1.0",
+author="Jason Joseph",
+packages= find_packages(),
+install_requires = get_requirements()
+
+)
